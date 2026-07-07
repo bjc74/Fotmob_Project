586c5411d2a57d953f2bb0ec9d819e8e47cb33f6
bjc74 <bjc74@cam.ac.uk>
2026-07-07 12:53:56 +0100

    added expected goals

--- match report.py
+++ match report.py
@@ -19,11 +19,15 @@ def match_report(match_id):
     team1_shots_on_target = team1_shots_saved + team1_goals
     team2_shots_on_target = team2_shots_saved + team2_goals
     top_players_by_events = get_top_players_by_events(events, 5)
+    top_xg_shots, xg_by_team = get_xg_stats(match_id)
+    team1_xg = xg_by_team.loc[team1]
+    team2_xg = xg_by_team.loc[team2]
     team_stats = pd.DataFrame([
         {
             "team":team1,
             "goals":team1_score,
             "shots":team1_shots,
+            "xg":team1_xg,
             "passes":team1_passes,
             "dribbles":team1_dribbles,
             "carries":team1_carries,
@@ -34,6 +38,7 @@ def match_report(match_id):
             "team":team2,
             "goals":team2_score,
             "shots":team2_shots,
+            "xg":team2_xg,
             "passes":team2_passes,
             "dribbles":team2_dribbles,
             "carries":team2_carries,
@@ -45,6 +50,7 @@ def match_report(match_id):
         {
             "team":team1,
             "total_shots":team1_shots,
+            "xg":team1_xg,
             "goals":team1_score,
             "shots_on_target":team1_shots_on_target,
             "shots_blocked":team1_shots_blocked,
@@ -53,6 +59,7 @@ def match_report(match_id):
         {
             "team":team2,
             "total_shots":team2_shots,
+            "xg":team2_xg,
             "goals":team2_score,
             "shots_on_target":team2_shots_on_target,
             "shots_blocked":team2_shots_blocked,
@@ -69,6 +76,7 @@ def match_report(match_id):
         "team_stats":team_stats,
         "shot_summary":shot_summary,
         "top_players":top_players_by_events,
+        "top_xg_shots":top_xg_shots
     }
 def get_score(events, team1, team2):
     team1_goals, team2_goals = count_shot_outcome_by_team("Goal", events, team1, team2)
@@ -103,56 +111,59 @@ def count_shot_outcome_by_team(shot_type, events, team1, team2):
     team2_score = goals_by_team.get(team2, 0)
     return team1_score, team2_score
 def print_match_report(report):
+    team_stats = report["team_stats"].set_index("team")
+    shot_summary = report["shot_summary"].set_index("team")
     team1, team2 = report["teams"]
     score = report["score"]
-    shots = report["team_stats"]["shots"]
-    passes = report["team_stats"]["passes"]
-    carries = report["team_stats"]["carries"]
-    pressures = report["team_stats"]["pressures"]
-    fouls_committed = report["team_stats"]["fouls_committed"]
     top_players = report["top_players"]
-    shots_on_target = report["shot_summary"]["shots_on_target"]
-    shots_off_target = report["shot_summary"]["shots_off_target"]
-    shots_blocked = report["shot_summary"]["shots_blocked"]
+    top_xg_shots = report["top_xg_shots"]
     print(f"Match: {team1} {score[team1]}-{score[team2]} {team2}")
     print("\nBasic stats: ")
     print(f"{team1}:")
-    print(f"- Shots: {shots.iloc[0]}")
+    print(f"- Shots: {team_stats.loc[team1, "shots"]}")
     print(f"- Goals: {score[team1]}")
-    print(f"- Passes: {passes.iloc[0]}")
-    print(f"- Carries: {carries.iloc[0]}")
-    print(f"- Pressures: {pressures.iloc[0]}")
-    print(f"- Fouls Committed: {fouls_committed.iloc[0]}")
+    print(f"- XG: {team_stats.loc[team1,"xg"]:.2f}")
+    print(f"- Passes: {team_stats.loc[team1, "passes"]}")
+    print(f"- Carries: {team_stats.loc[team1, "carries"]}")
+    print(f"- Pressures: {team_stats.loc[team1, "pressures"]}")
+    print(f"- Fouls Committed: {team_stats.loc[team1, "fouls_committed"]}")
     print(f"\n{team2}:")
-    print(f"- Shots: {shots.iloc[1]}")
+    print(f"- Shots: {team_stats.loc[team2, "shots"]}")
     print(f"- Goals: {score[team2]}")
-    print(f"- Passes: {passes.iloc[1]}")
-    print(f"- Carries: {carries.iloc[1]}")
-    print(f"- Pressures: {pressures.iloc[1]}")
-    print(f"- Fouls Committed: {fouls_committed.iloc[1]}")
+    print(f"- XG: {team_stats.loc[team2, "xg"]:.2f}")
+    print(f"- Passes: {team_stats.loc[team2, "passes"]}")
+    print(f"- Carries: {team_stats.loc[team2, "carries"]}")
+    print(f"- Pressures: {team_stats.loc[team2, "pressures"]}")
+    print(f"- Fouls Committed: {team_stats.loc[team2, "fouls_committed"]}")
     print("\nTop players:")
     print(top_players.to_string(index=False))
+    print("\nTop XG shots:")
+    print(top_xg_shots.to_string(index = False))
     print(f"\nShot Summary:\n{team1}:")
-    print(f"- Total Shots: {shots.iloc[0]}")
+    print(f"- Total Shots: {shot_summary.loc[team1, "total_shots"]}")
+    print(f"- XG: {shot_summary.loc[team1, "xg"]:.2f}")
     print(f"- Goals: {score[team1]}")
-    print(f"- Shots On Target: {shots_on_target.iloc[0]}")
-    print(f"- Shots Off Target: {shots_off_target.iloc[0]}")
-    print(f"- Shots Blocked: {shots_blocked.iloc[0]}")
+    print(f"- Shots On Target: {shot_summary.loc[team1, "shots_on_target"]}")
+    print(f"- Shots Off Target: {shot_summary.loc[team1, "shots_off_target"]}")
+    print(f"- Shots Blocked: {shot_summary.loc[team1, "shots_blocked"]}")
     print(f"\n{team2}: ")
-    print(f"- Total Shots: {shots.iloc[1]}")
+    print(f"- Total Shots: {shot_summary.loc[team2, "total_shots"]}")
+    print(f"- XG: {shot_summary.loc[team2, "xg"]:.2f}")
     print(f"- Goals: {score[team2]}")
-    print(f"- Shots On Target: {shots_on_target.iloc[1]}")
-    print(f"- Shots Off Target: {shots_off_target.iloc[1]}")
-    print(f"- Shots Blocked: {shots_blocked.iloc[1]}")
+    print(f"- Shots On Target: {shot_summary.loc[team2, "shots_on_target"]}")
+    print(f"- Shots Off Target: {shot_summary.loc[team2, "shots_off_target"]}")
+    print(f"- Shots Blocked: {shot_summary.loc[team2, "shots_blocked"]}")
 def export_report(report, output_dir="outputs"):
     os.makedirs(output_dir, exist_ok=True)
     match_id = report["match_id"]
     team_stats_path = os.path.join(output_dir, f"match_{match_id}_team_stats.csv")
     shot_summary_path = os.path.join(output_dir, f"match_{match_id}_shot_summary.csv")
     top_players_path = os.path.join (output_dir, f"match_{match_id}_top_players.csv")
+    top_xg_shots_path = os.path.join(output_dir, f"match_{match_id}_top_xg_shots.csv")
     team_stats_csv = report["team_stats"].to_csv(team_stats_path, index = False)
     shot_summary_csv = report["shot_summary"].to_csv(shot_summary_path, index = False)
     top_players_csv = report["top_players"].to_csv(top_players_path, index = False)
+    top_xg_shots_csv = report["top_xg_shots"].to_csv(top_xg_shots_path, index = False)
 def validate_scores(competition_id, season_id, n=20):
     matches = sb.matches(competition_id=competition_id, season_id=season_id)
     errors = []
@@ -166,7 +177,7 @@ def validate_scores(competition_id, season_id, n=20):
         score = report["score"]
         home_score_calculated = score.get(home_team)
         away_score_calculated = score.get(away_team)
-        if home_score_offical != home_score_calculated and away_score_official != away_score_calculated:
+        if home_score_offical != home_score_calculated or away_score_official != away_score_calculated:
             errors.append({
                 "match_id":match_id,
                 "home_team":home_team,
@@ -177,11 +188,6 @@ def validate_scores(competition_id, season_id, n=20):
                 "away_score_calculated":away_score_calculated
             })
     return pd.DataFrame(errors)
-def match_report_from_competition(competition_id, season_id, match_id):
-    matches = sb.matches(competition_id=competition_id, season_id=season_id)
-    match = matches[matches["match_id"] == match_id].iloc[0]
-    home_team = match["home_team"]
-    away_team = match["away_team"]
     return match_report(match_id, home_team=home_team, away_team=away_team)
 def plot_team_stats(report, output_dir="outputs"):
     match_id = report["match_id"]
@@ -198,21 +204,19 @@ def plot_team_stats(report, output_dir="outputs"):
     return normalised_stats_by_metric
 def normalise_stats_by_metric(report):
     team1,team2 = report["teams"]
-    shots = report["team_stats"]["shots"]
-    carries = report["team_stats"]["carries"]
-    pressures = report["team_stats"]["pressures"]
-    passes = report["team_stats"]["passes"]
-    fouls_committed = report["team_stats"]["fouls_committed"]
-    team1_normalised_shots = shots.iloc[0]/(shots.iloc[0] + shots.iloc[1]) *100
+    team_stats = report["team_stats"].set_index("team")
+    team1_normalised_shots = team_stats.loc[team1, "shots"] /(team_stats.loc[team1, "shots"] + team_stats.loc[team2, "shots"]) *100
     team2_normalised_shots = 100 - team1_normalised_shots
-    team1_normalised_carries = carries.iloc[0]/(carries.iloc[0]+carries.iloc[1]) *100
+    team1_normalised_carries = team_stats.loc[team1, "carries"]/(team_stats.loc[team1, "carries"] + team_stats.loc[team2, "carries"])*100
     team2_normalised_carries = 100 - team1_normalised_carries
-    team1_normalised_pressures = pressures.iloc[0]/(pressures.iloc[0]+pressures.iloc[1])*100
+    team1_normalised_pressures = team_stats.loc[team1, "pressures"]/(team_stats.loc[team1, "pressures"] + team_stats.loc[team2, "pressures"])*100
     team2_normalised_pressures = 100 - team1_normalised_pressures
-    team1_normalised_passes = passes.iloc[0]/(passes.iloc[0]+passes.iloc[1])*100
+    team1_normalised_passes = team_stats.loc[team1, "passes"]/(team_stats.loc[team1, "passes"] + team_stats.loc[team2, "passes"])*100
     team2_normalised_passes = 100 - team1_normalised_passes
-    team1_normalised_fouls_committed = fouls_committed.iloc[0]/(fouls_committed.iloc[0]+fouls_committed.iloc[1])*100
+    team1_normalised_fouls_committed = team_stats.loc[team1, "fouls_committed"]/(team_stats.loc[team1, "fouls_committed"]+team_stats.loc[team2, "fouls_committed"])*100
     team2_normalised_fouls_committed = 100 - team1_normalised_fouls_committed
+    team1_normalised_xg = team_stats.loc[team1, "xg"]/(team_stats.loc[team1, "xg"] + team_stats.loc[team2, "xg"])*100
+    team2_normalised_xg = 100 - team1_normalised_xg
     stats_by_metric = ({
         "normalised_shots":{
             team1:team1_normalised_shots,
@@ -233,11 +237,23 @@ def normalise_stats_by_metric(report):
         "normalised_fouls_committed":{
             team1:team1_normalised_fouls_committed,
             team2:team2_normalised_fouls_committed
+        },
+        "normalised_xg":{
+            team1:team1_normalised_xg,
+            team2:team2_normalised_xg
         }
     })
     return pd.DataFrame(stats_by_metric).T
+def get_xg_stats(match_id):
+    events = sb.events(match_id=match_id)
+    shots = events[events["type"] == "Shot"]
+    shots = shots[["player", "team", "minute", "shot_statsbomb_xg", "shot_outcome"]]
+    expected_goals_by_team = shots.groupby("team")["shot_statsbomb_xg"].sum()
+    sorted_shots = shots.sort_values("shot_statsbomb_xg", ascending = False).head()
+    return sorted_shots, expected_goals_by_team
 report = match_report(3749493)
+get_xg_stats(3749493)
 print_match_report(report)
 export_report(report, output_dir="outputs")
-print(validate_scores(2, 44))
+#validate_scores(2, 44)
 plot_team_stats(report)

78fb6245d9e342c77736629aa9e2c53000a58f92
bjc74 <bjc74@cam.ac.uk>
2026-07-14 22:52:03 +0100

    setting up pass network

--- match report.py
+++ match report.py
@@ -21,6 +21,10 @@ def match_report(match_id):
     top_players_by_events = get_top_players_by_events(events, 5)
     top_on_ball_contributors = get_attacking_stats(match_id=match_id)
     top_xg_shots, xg_by_team = get_xg_stats(match_id)
+    xg_timeline = get_xg_timeline(match_id=match_id)
+    shot_map_data = get_shot_map_data(match_id=match_id)
+    pass_combinations= get_pass_combinations(match_id)
+    pass_network = get_pass_network_data(match_id)
     team1_xg = xg_by_team.loc[team1]
     team2_xg = xg_by_team.loc[team2]
     team_stats = pd.DataFrame([
@@ -78,7 +82,11 @@ def match_report(match_id):
         "shot_summary":shot_summary,
         "top_players":top_players_by_events,
         "top_xg_shots":top_xg_shots,
-        "top_on_ball_contributors":top_on_ball_contributors
+        "top_on_ball_contributors":top_on_ball_contributors,
+        "xg_timeline":xg_timeline,
+        "shot_map_data":shot_map_data,
+        "pass_combinations":pass_combinations,
+        'pass_network':pass_network
     }
 def get_score(events, team1, team2):
     team1_goals, team2_goals = count_shot_outcome_by_team("Goal", events, team1, team2)
@@ -170,11 +178,19 @@ def export_report(report, output_dir="outputs"):
     top_players_path = os.path.join (output_dir, f"match_{match_id}_top_players.csv")
     top_xg_shots_path = os.path.join(output_dir, f"match_{match_id}_top_xg_shots.csv")
     top_on_ball_contributors_path = os.path.join(output_dir, f"match_{match_id}_top_on_ball_contributors.csv")
+    xg_timeline_path = os.path.join(output_dir, f"match_{match_id}_xg_timeline.csv")
+    shot_map_data_path = os.path.join(output_dir, f"match_{match_id}_shot_map_data.csv")
+    pass_combinations_path = os.path.join(output_dir, f'match_{match_id}_pass_combinations.csv')
+    pass_network_path = os.path.join(output_dir, f'match_{match_id}_pass_network.csv')
     team_stats_csv = report["team_stats"].to_csv(team_stats_path, index = False)
     shot_summary_csv = report["shot_summary"].to_csv(shot_summary_path, index = False)
     top_players_csv = report["top_players"].to_csv(top_players_path, index = False)
     top_xg_shots_csv = report["top_xg_shots"].to_csv(top_xg_shots_path, index = False)
     top_on_ball_contributors_csv = report["top_on_ball_contributors"].to_csv(top_on_ball_contributors_path, index = False)
+    xg_timeline_csv = report["xg_timeline"].to_csv(xg_timeline_path, index = False)
+    shot_map_data_csv = report['shot_map_data'].to_csv(shot_map_data_path, index = False)
+    pass_combinations_csv = report['pass_combinations'].to_csv(pass_combinations_path, index = False)
+    pass_network_csv = report['pass_network'].to_csv(pass_network_path, index = False)
 def validate_scores(competition_id, season_id, n=20):
     matches = sb.matches(competition_id=competition_id, season_id=season_id)
     errors = []
@@ -211,8 +227,36 @@ def plot_team_stats(report, output_dir="outputs"):
     plt.title(f"Team event share by metric — match {match_id}")
     plt.tight_layout()
     plt.savefig(plot_team_stats_path)
+    xg_race = report['xg_timeline']
+    xg_race['time'] = (xg_race['minute'] + (xg_race['second']/60))
+    xg_race = xg_race[['time','team','cumulative_xg']]
+    ax = xg_race[xg_race['team'] == team1].plot.line(x = 'time', y = 'cumulative_xg', rot = 0, label = team1)
+    xg_race[xg_race['team'] == team2].plot.line(x = 'time', y = 'cumulative_xg', rot = 0, ax = ax, label = team2)
+    xg_race_path = os.path.join(output_dir, f'match_{match_id}_xg_race.png')
+    plt.ylabel("Cumulative XG")
+    plt.title(f"Xg race between teams - match {match_id}")
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(xg_race_path)
+    shot_map_path = os.path.join(output_dir, f'match_{match_id}_shot_map.png')
+    shot_map_data = report['shot_map_data']
+    shot_map_data['point_sizes'] = shot_map_data['shot_statsbomb_xg']*500
+    ax = shot_map_data[shot_map_data['team'] == team1].plot.scatter(x = 'x', y = 'y', s = 'point_sizes', label = team1, color = 'red')
+    shot_map_data[shot_map_data['team'] == team2].plot.scatter(x = 'x', y = 'y',s = 'point_sizes', label = team2, ax = ax, color = 'blue')
+    shot_map_data[shot_map_data['shot_outcome'] == 'Goal'].plot.scatter(x = 'x', y = 'y',s = 'point_sizes', label = 'Goal', ax = ax, marker = '*', color = 'green')
+    plt.ylabel('y location')
+    plt.xlabel('x location')
+    plt.title(f'Shot Map - match {match_id}')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(shot_map_path)
+    pass_network_data_path  =os.path.join(output_dir, f'match_{match_id}_pass_network.png')
+    pass_network_data = report['pass_network']
+    ax1 = pass_network_data.plot.scatter(x = 'avg_x_passed', y = 'avg_y_passed', s = 'total_passes')
+    for _, row in pass_network_data.iterrows():
+        ax1.annotate(row["player"],(row["avg_x_passed"], row["avg_y_passed"]),fontsize=8)
+    plt.legend()
     plt.show()
-    return normalised_stats_by_metric
 def normalise_stats_by_metric(report):
     team1,team2 = report["teams"]
     team_stats = report["team_stats"].set_index("team")
@@ -271,9 +315,44 @@ def get_attacking_stats(match_id):
     events = events.reset_index()
     sorted_events = events.sort_values('total_attacking_stats', ascending=False)
     return sorted_events
+def get_xg_timeline(match_id):
+    events = sb.events(match_id = match_id)
+    shots = events[events["type"] == "Shot"]
+    shots = shots[["minute", "second", "team", "player", "shot_statsbomb_xg", "shot_outcome"]]
+    shots["cumulative_xg"] = shots.groupby("team")["shot_statsbomb_xg"].cumsum()
+    return shots
+def get_shot_map_data(match_id):
+    events = sb.events(match_id=match_id)
+    shots = events[events['type'] == 'Shot']
+    shots['x'] = shots['location'].str[0]
+    shots['y'] = shots['location'].str[1]
+    shots = shots[['player', 'team', 'minute','x','y', 'shot_statsbomb_xg', 'shot_outcome']]
+    return shots
+def get_pass_combinations(match_id):
+    events = sb.events(match_id)
+    passes = events[events['type'] == 'Pass']
+    passes = passes[['team', 'player', 'pass_recipient']]
+    pass_combinations = passes.value_counts()
+    pass_combinations = pass_combinations.reset_index(name = 'pass_count')
+    pass_combinations = pass_combinations.rename(columns={'player':'passer', 'pass_recipient':'recipient'})
+    pass_combinations = pass_combinations.sort_values(['team', 'pass_count'], ascending=[True, False])
+    return pass_combinations
+def get_pass_network_data(match_id):
+    events = sb.events(match_id)
+    passes = events[events['type'] == 'Pass']
+    pass_network = passes[['team', 'player', 'location', 'pass_end_location']]
+    pass_network['x_passed'] = pass_network['location'].str[0]
+    pass_network['y_passed'] = pass_network['location'].str[1]
+    pass_network['x_recieved'] = pass_network['pass_end_location'].str[0]
+    pass_network['y_recieved'] = pass_network['pass_end_location'].str[1]
+    pass_network = pass_network[['team', 'player','x_passed', 'y_passed', 'x_recieved', 'y_recieved']]
+    pass_network = pass_network.groupby(['team','player']).agg(avg_x_passed = ('x_passed', 'mean'), avg_y_passed = ('y_passed','mean'), avg_x_recieved = ('x_recieved','mean'), avg_y_recieved = ('y_recieved', 'mean'), total_passes = ('player', 'size')).reset_index()
+    return pass_network
 report = match_report(3749493)
-print_match_report(report)
+#print_match_report(report)
 export_report(report, output_dir="outputs")
 #validate_scores(2, 44)
 plot_team_stats(report)
+get_pass_network_data(3749493)
+
 
